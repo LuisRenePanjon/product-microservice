325fade3f779565f335826a211958dcc70dfe0da
Luis René Panjón <luchorene97@gmail.com>
2022-11-03 17:56:06 -0500

    Fix: Añadimos notación Id a la entidad

--- src/main/java/com/spring/productmicroservice/entity/ProductEntity.java
+++ src/main/java/com/spring/productmicroservice/entity/ProductEntity.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter @Setter
@@ -13,6 +14,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(value = "product")
 public class ProductEntity {
 
+    @Id
     private String id;
     private String productName;
     private String productDescription;
