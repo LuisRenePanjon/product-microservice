e0a17f062dc432c7394dded2ab0e56ced6e30532
Luis René Panjón <luchorene97@gmail.com>
2022-11-03 17:18:00 -0500

    Creación de proyecto de Spring

---
+++ src/main/java/com/spring/productmicroservice/SpringMicrosApplication.java
@@ -0,0 +1,13 @@
+package com.spring.productmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicrosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMicrosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/productmicroservice/SpringMicrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.productmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicrosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
