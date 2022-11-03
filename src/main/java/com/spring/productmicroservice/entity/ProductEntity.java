96ba63335be31fe0edcb28ee83d5a9a359715c45
Luis René Panjón <luchorene97@gmail.com>
2022-11-03 17:36:20 -0500

    Creación de entidad de product

---
+++ src/main/java/com/spring/productmicroservice/entity/ProductEntity.java
@@ -0,0 +1,22 @@
+package com.spring.productmicroservice.entity;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter @Setter
+@ToString
+@NoArgsConstructor
+@Document(value = "product")
+public class ProductEntity {
+
+    private String id;
+    private String productName;
+    private String productDescription;
+    private Double unitPrice;
+
+
+}
