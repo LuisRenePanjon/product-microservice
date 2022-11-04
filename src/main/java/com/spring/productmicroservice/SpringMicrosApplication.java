9c4af45ef1420bbf5f68ff1e649231e4c94720c0
Luis René Panjón <luchorene97@gmail.com>
2022-11-04 02:48:14 -0500

    Añadimos notación para habilitar el cliente de Eureka

--- src/main/java/com/spring/productmicroservice/SpringMicrosApplication.java
+++ src/main/java/com/spring/productmicroservice/SpringMicrosApplication.java
@@ -2,8 +2,10 @@ package com.spring.productmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringMicrosApplication {
 
     public static void main(String[] args) {
