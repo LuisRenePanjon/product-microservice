eda676a23bbf28175c63eebfd20b2ededaffcfbf
Luis René Panjón <luchorene97@gmail.com>
2022-11-05 18:48:46 -0500

    Crear controlador de category para probar actuator

---
+++ src/main/java/com/spring/productmicroservice/controller/CategoryController.java
@@ -0,0 +1,28 @@
+package com.spring.productmicroservice.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@Setter @Getter
+@RefreshScope
+public class CategoryController {
+
+    @Value("${app.testProp}")
+    private String testProps;
+
+    @GetMapping("/test")
+    @ResponseStatus(HttpStatus.OK)
+    public String getTestProps() {
+        return this.testProps;
+    }
+
+}
