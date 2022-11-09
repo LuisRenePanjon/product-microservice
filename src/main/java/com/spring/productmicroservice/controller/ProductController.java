2a06a5558f7736480a9352a390f14c3d3ee45967
Luis René Panjón <luchorene97@gmail.com>
2022-11-08 19:51:49 -0500

    Cambiar path de Controller de products

--- src/main/java/com/spring/productmicroservice/controller/ProductController.java
+++ src/main/java/com/spring/productmicroservice/controller/ProductController.java
@@ -11,26 +11,26 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/products")
 public class ProductController {
 
     @Autowired
     private ProductRepository productRepository;
 
 
-    @GetMapping("/products")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<ProductEntity> getAllProducts(){
         return productRepository.findAll(Sort.sort(ProductEntity.class).by(ProductEntity::getProductName).ascending());
     }
 
-    @PostMapping("/products")
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductEntity productEntity){
         productRepository.save(productEntity);
     }
 
-    @DeleteMapping("/products/{id}")
+    @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteProduct(@PathVariable String id){
         productRepository.deleteById(id);
